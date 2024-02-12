ea7dc2da011262fb9044d5cb3e7f0e70504cb86f
EC2 Default User <ec2-user@ip-172-31-90-17.ec2.internal>
2024-02-12 16:16:00 +0000

    Added the new version of source-healthie-api

---
+++ source_healthie_api/graph_ql_queries.py
@@ -0,0 +1,305 @@
+users_query = """
+            query users(
+                $offset: Int,
+                $keywords: String,
+                $sort_by: String,
+                $active_status: String,
+                $group_id: String,
+                $show_all_by_default: Boolean,
+                $should_paginate: Boolean,
+                $provider_id: String,
+                $conversation_id: ID,
+                $limited_to_provider: Boolean,
+                ) {
+                usersCount(
+                    keywords: $keywords,
+                    active_status:$active_status,
+                    group_id: $group_id,
+                    conversation_id: $conversation_id,
+                    provider_id: $provider_id,
+                    limited_to_provider: $limited_to_provider
+                )
+                users(
+                    offset: $offset,
+                    keywords: $keywords,
+                    sort_by: $sort_by,
+                    active_status: $active_status,
+                    group_id: $group_id,
+                    conversation_id: $conversation_id,
+                    show_all_by_default: $show_all_by_default,
+                    should_paginate: $should_paginate,
+                    provider_id: $provider_id,
+                    limited_to_provider: $limited_to_provider
+                ) {
+                    id
+                }
+            }
+        """
+
+appointment_types_query = """
+            query getAppointmentTypes(
+                $offset: Int,
+                $should_paginate: Boolean,
+                $page_size: Int,
+                $keywords: String,
+                $show_group: Boolean,
+                $provider_id: String,
+                $clients_can_book: Boolean,
+                $appointment_type_ids: String,
+                $with_deleted_appt_types: Boolean
+            ) {
+                appointmentTypes(
+                    offset: $offset,
+                    should_paginate: $should_paginate,
+                    page_size: $page_size,
+                    keywords: $keywords,
+                    show_group: $show_group,
+                    provider_id: $provider_id,
+                    clients_can_book: $clients_can_book,
+                    appointment_type_ids: $appointment_type_ids,
+                    with_deleted_appt_types: $with_deleted_appt_types
+                ) {
+                    id
+                    name
+                    available_contact_types
+                    length
+                    is_group
+                    is_waitlist_enabled
+                }
+            }
+        """
+
+appointments_query = """
+query appointments(
+  $user_id: ID,
+  $filter: String,
+  $sort_by: String,
+  $should_paginate: Boolean,
+  $offset: Int,
+  $is_active: Boolean,
+  $with_all_statuses: Boolean
+) {
+  appointmentsCount(user_id: $user_id, filter: $filter, is_org: true, is_active: $is_active)
+  appointments(
+    is_active: $is_active,
+    user_id: $user_id,
+    filter: $filter,
+    is_org: true,
+    sort_by: $sort_by,
+    should_paginate: $should_paginate,
+    offset: $offset,
+    with_all_statuses: $with_all_statuses
+  ) {
+    id
+    date
+    contact_type
+    length
+    location
+    provider {
+      id
+      full_name
+    }
+
+    appointment_type {
+      name
+      id
+    }
+
+    attendees {
+      id
+      full_name
+      first_name
+      avatar_url
+      phone_number
+    }
+  }
+}
+"""
+
+available_item_types_query = """
+query getAvailableItemTypes(
+  $onboarding_flow_id: String
+) {
+  availableItemTypes(
+    onboarding_flow_id: $onboarding_flow_id
+  )
+}
+"""
+
+conversations_query = """
+query conversationMemberships(
+  $offset: Int
+  $keywords: String
+  $active_status: String
+  $client_id: String
+  $read_status: String
+  $conversation_type: String
+  $provider_id: ID
+) {
+  conversationMembershipsCount(
+    keywords: $keywords
+    active_status: $active_status
+    client_id: $client_id
+    read_status: $read_status
+    conversation_type: $conversation_type
+    provider_id: $provider_id
+  )
+  conversationMemberships(
+    offset: $offset
+    keywords: $keywords
+    active_status: $active_status
+    client_id: $client_id
+    read_status: $read_status
+    conversation_type: $conversation_type
+    provider_id: $provider_id
+  ) {
+    id
+    display_name
+    archived
+    viewed
+    convo {
+      id
+      conversation_memberships_count
+    }
+  }
+}
+"""
+
+form_completion_requests_query = """
+query requestedFormCompletions(
+  $userId: ID,
+  $keywords: String,
+  $status: String
+) {
+  requestedFormCompletions(
+    user_id: $userId,
+    keywords: $keywords,
+    status: $status
+  ) {
+    id
+    item_type
+  }
+}
+"""
+
+forms_query = """
+query formTemplates(
+  $include_default_templates: Boolean
+  $active_status: Boolean
+  $should_paginate: Boolean
+  $category: String
+  $keywords: String
+  $offset: Int
+  $sortBy: String
+) {
+  customModuleForms(
+    include_default_templates: $include_default_templates
+    active_status: $active_status
+    should_paginate: $should_paginate
+    category: $category
+    keywords: $keywords
+    offset: $offset
+    sort_by: $sortBy
+  ) {
+    id
+    is_video
+    name
+    prefill
+    uploaded_by_healthie_team
+  }
+}
+"""
+
+onboarding_flows_query = """
+query onboardingFlows(
+  $offset: Int,
+  $keywords: String,
+  $sort_by: String,
+  $should_paginate: Boolean
+) {
+  onboardingFlows(
+    offset: $offset,
+    keywords: $keywords,
+    sort_by: $sort_by,
+    should_paginate: $should_paginate
+  ) {
+    id
+    name
+    is_multiple_providers
+    has_forms_after_welcome
+  }
+}
+"""
+
+organization_members_query = """
+query getOrganizationMembers(
+    $offset: Int,
+    $keywords: String,
+    $sort_by: String,
+    $licensed_in_state: String,
+    $conversation_id: ID
+) {
+  organizationMembers(
+    offset: $offset,
+    keywords: $keywords,
+    sort_by: $sort_by,
+    licensed_in_state: $licensed_in_state,
+    conversation_id: $conversation_id
+  ) {
+    id
+    first_name
+    last_name
+    email
+    has_api_access
+  }
+}
+"""
+
+programs_query = """
+query courses(
+  $offset: Int,
+  $keywords: String,
+  $course_type: String,
+  $should_paginate: Boolean,
+  $only_available: Boolean
+) {
+  courses(
+    offset: $offset,
+    keywords: $keywords,
+    course_type: $course_type,
+    should_paginate: $should_paginate,
+    only_available: $only_available
+  ) {
+    id
+    name
+    description
+    course_type
+    course_items {
+      id
+      category
+      item_type
+      item_id
+    }
+    start_date
+  }
+}
+"""
+
+unassociated_completed_onboarding_items_query = """
+query getUnassociatedCompletedOnboardingItems(
+  $user_id: ID
+) {
+  unassociatedCompletedOnboardingItems(
+    user_id: $user_id
+  ) {
+    id
+    item_type
+    item_id
+    onboarding_item_id
+    skipped
+    user {
+      id
+    }
+  }
+}
+"""
---
+++ source_healthie_api/source.py
@@ -0,0 +1,562 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+from abc import ABC
+from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+
+import requests
+import json
+from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator, NoAuth
+
+from .graph_ql_queries import (
+    users_query,
+    appointment_types_query,
+    appointments_query,
+    available_item_types_query,
+    conversations_query,
+    form_completion_requests_query,
+    forms_query,
+    onboarding_flows_query,
+    organization_members_query,
+    programs_query,
+    unassociated_completed_onboarding_items_query,
+)
+
+
+
+# Basic full refresh stream
+class HealthieApiStream(HttpStream, ABC):
+    url_base = "https://staging-api.gethealthie.com/graphql"
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        """
+        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
+
+        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
+        to most other methods in this class to help you form headers, request bodies, query params, etc..
+
+        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
+        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
+        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
+
+        :param response: the most recent response from the API
+        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
+                If there are no more pages in the result, return None.
+        """
+        return None
+    
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return ""
+    
+    @property
+    def http_method(self) -> str:
+        return "POST"
+
+
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        """
+        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
+        Usually contains common params e.g. pagination size etc.
+        """
+        return None
+    
+    def request_headers(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> Mapping[str, Any]:
+        return {
+            'AuthorizationSource': 'API',
+            'Content-Type': 'application/json',    
+        }
+
+
+class Users(HealthieApiStream):
+    primary_key = "id"
+
+    def __init__(self, authenticator):
+        super().__init__(authenticator)
+        self.current_offset = 0
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        current_data = response.json().get("data", {}).get("users", [])
+        if not current_data:
+            return None
+        
+        if self.current_offset > 30:
+            return None
+        
+        self.current_offset += len(current_data)
+        return {"offset": self.current_offset}
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        users = response.json().get("data").get("users")
+        return users
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+        
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+        
+        return {"query": users_query, "variables": variables}
+
+
+class AppointmentTypes(HealthieApiStream):
+    primary_key = "id"
+
+    def __init__(self, authenticator):
+        super().__init__(authenticator)
+        self.current_offset = 0
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        current_data = response.json().get("data", {}).get("appointmentTypes", [])
+        if not current_data:
+            return None
+        
+        if self.current_offset > 30:
+            return None
+        
+        self.current_offset += len(current_data)
+        return {"offset": self.current_offset}
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json().get("data").get("appointmentTypes")
+        return data
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+        
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+
+        return {"query": appointment_types_query, "variables": variables}
+
+
+class Appointments(HealthieApiStream):
+    primary_key = "id"
+
+    def __init__(self, authenticator):
+        super().__init__(authenticator)
+        self.current_offset = 0
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        current_data = response.json().get("data", {}).get("appointments", [])
+        if not current_data:
+            return None
+        
+        if self.current_offset > 30:
+            return None
+        
+        self.current_offset += len(current_data)
+        return {"offset": self.current_offset}
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json().get("data").get("appointments")
+        return data
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+        
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+
+        return {"query": appointments_query, "variables": variables}
+
+
+class AvailableItemTypes(HealthieApiStream):
+    primary_key = "id"
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json().get("data").get("availableItemTypes")
+        json_data = json.loads(data)
+        return json_data
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+
+        return {"query": available_item_types_query, "variables": variables}
+
+
+class Conversations(HealthieApiStream):
+    primary_key = "id"
+
+    def __init__(self, authenticator):
+        super().__init__(authenticator)
+        self.current_offset = 0
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        current_data = response.json().get("data", {}).get("conversationMemberships", [])
+        if not current_data:
+            return None
+        
+        if self.current_offset > 30:
+            return None
+        
+        self.current_offset += len(current_data)
+        return {"offset": self.current_offset}
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json().get("data").get("conversationMemberships")
+        return data
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+        
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+
+        return {"query": conversations_query, "variables": variables}
+
+
+class FormCompletionRequests(HealthieApiStream):
+    primary_key = "id"
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json().get("data").get("requestedFormCompletions")
+        return data
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+        
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+
+        return {"query": form_completion_requests_query, "variables": variables}
+
+
+class Forms(HealthieApiStream):
+    primary_key = "id"
+
+    def __init__(self, authenticator):
+        super().__init__(authenticator)
+        self.current_offset = 0
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        current_data = response.json().get("data", {}).get("customModuleForms", [])
+        if not current_data:
+            return None
+        
+        if self.current_offset > 30:
+            return None
+        
+        self.current_offset += len(current_data)
+        return {"offset": self.current_offset}
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json().get("data").get("customModuleForms")
+        return data
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+
+        return {"query": forms_query, "variables": variables}
+
+
+class OnboardingFlows(HealthieApiStream):
+    primary_key = "id"
+
+    def __init__(self, authenticator):
+        super().__init__(authenticator)
+        self.current_offset = 0
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        current_data = response.json().get("data", {}).get("onboardingFlows", [])
+        if not current_data:
+            return None
+        
+        if self.current_offset > 30:
+            return None
+        
+        self.current_offset += len(current_data)
+        return {"offset": self.current_offset}
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json().get("data").get("onboardingFlows")
+        return data
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+
+        return {"query": onboarding_flows_query, "variables": variables}
+
+
+class OrganizationMembers(HealthieApiStream):
+    primary_key = "id"
+
+    def __init__(self, authenticator):
+        super().__init__(authenticator)
+        self.current_offset = 0
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        current_data = response.json().get("data", {}).get("organizationMembers", [])
+        if not current_data:
+            return None
+        
+        if self.current_offset > 30:
+            return None
+        
+        self.current_offset += len(current_data)
+        return {"offset": self.current_offset}
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json().get("data").get("organizationMembers")
+        return data
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+        
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+
+        return {"query": organization_members_query, "variables": variables}
+
+
+class Programs(HealthieApiStream):
+    primary_key = "id"
+
+    def __init__(self, authenticator):
+        super().__init__(authenticator)
+        self.current_offset = 0
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        current_data = response.json().get("data", {}).get("courses", [])
+        if not current_data:
+            return None
+        
+        if self.current_offset > 30:
+            return None
+        
+        self.current_offset += len(current_data)
+        return {"offset": self.current_offset}
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json().get("data").get("courses")
+        return data
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+        
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+
+        return {"query": programs_query, "variables": variables}
+
+
+class UnassociatedCompletedOnboardingItems(HealthieApiStream):
+    primary_key = "id"
+    
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        data = response.json().get("data").get("unassociatedCompletedOnboardingItems")
+        return data
+
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]:
+
+        variables = {}
+        if next_page_token:
+            variables = next_page_token
+
+        return {"query": unassociated_completed_onboarding_items_query, "variables": variables}
+
+
+# Source
+class SourceHealthieApi(AbstractSource):
+    def check_connection(self, logger, config) -> Tuple[bool, any]:
+        """
+        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
+
+        See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
+        for an example.
+
+        :param config:  the user-input config object conforming to the connector's spec.yaml
+        :param logger:  logger object
+        :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
+        """
+        try:
+            graph_ql_query = """
+                query currentUser {
+                    currentUser {
+                        id
+                    }
+                }
+            """
+
+
+            data = {"query": graph_ql_query, "variables": {}}
+            api_key = config.get("apikey")
+            headers = {
+                "Authorization": f"Basic {api_key}",
+                "Content-Type": "application/json",
+                "AuthorizationSource": "API"
+            }
+
+            response = requests.post(url="https://staging-api.gethealthie.com/graphql", json=data, headers=headers)
+            data = response.json()
+            logger.info(f"Data: {data}")
+            if data.get("errors"):
+                logger.error(f"Request was not successfull")
+                return False, data.get("errors")
+            
+            return True, None
+        
+        except Exception as e:
+            logger.info(f"Request was not successfull")
+            return False, "Unable to connect to the API with the provided credentials"
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        """
+        TODO: Replace the streams below with your own streams.
+
+        :param config: A Mapping of the user input configuration as defined in the connector spec.
+        """
+        auth = TokenAuthenticator(token=config.get("apikey"), auth_method="Basic")
+        return [
+            Users(authenticator=auth),
+            AppointmentTypes(authenticator=auth),
+            Appointments(authenticator=auth),
+            AvailableItemTypes(authenticator=auth),
+            Conversations(authenticator=auth),
+            FormCompletionRequests(authenticator=auth),
+            Forms(authenticator=auth),
+            OnboardingFlows(authenticator=auth),
+            OrganizationMembers(authenticator=auth),
+            Programs(authenticator=auth),
+            UnassociatedCompletedOnboardingItems(authenticator=auth),
+        ]
